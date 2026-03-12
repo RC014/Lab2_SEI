ebef0deb9d7335d8e760af34e1fa415d7982c8be
Stefan Mandrescu <stefanmandrescu1401@gmail.com>
2026-03-12 15:42:52 +0200

    added FreeRTOS lib

--- lib/dd_serial_stdio/dd_serial_stdio.h
+++ lib/dd_serial_stdio/dd_serial_stdio.h
@@ -2,7 +2,7 @@
 #define DD_SERIAL_STDIO_H_
 #include <stdio.h>
 #include <Arduino_FreeRTOS.h>
-#include "semphr.h"
+#include "../semphr/semphr.h"
 int srv_serial_put_char(char ch, FILE *f);
 int srv_serial_get_char(FILE *f);
 void safe_printf(const char* format, ...);
